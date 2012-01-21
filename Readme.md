c81889dc649f0eefccdb3d9229b240aa9560c7f0
Ariel Flesler <aflesler@gmail.com>
2012-01-20 21:22:25 -0300

    Added basic Readme and stub js file

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# HashMap Class for JavaScript
+
+## Description
+
+This scripts provides you with a HashMap class that works both on NodeJS and the browser.
+The HashMap accepts any type of key and won't stringify it.
+
+Numbers and strings won't be mixed. You can pass Date objects, RegExps, DOM Elements, anything!
---
+++ hashmap.js
@@ -0,0 +1,26 @@
+/**
+ * NOTE: I haven't really started coding this
+ * Will start ASAP!
+ *
+ * @author Ariel Flesler <aflesler@gmail.com>
+ */
+
+;(function(exports){
+	
+	function HashMap(hashProvider) {
+		
+	}
+
+	function ExpandoHashProvider() {
+		
+	}
+
+	function ArrayHashProvider() {
+		
+	}
+
+	exports.HashMap = HashMap;
+	exports.ExpandoHashProvider = ExpandoHashProvider;
+	exports.ArrayHashProvider = ArrayHashProvider;
+
+})(this.exports || this);
