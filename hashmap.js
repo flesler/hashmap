9639793f9c9f15da5dd7fe0f5aac6492cfc98303
Abbey Sparrow <asparrow@asparrow-MBR.local>
2014-06-14 08:24:21 -0700

    made it UMD

--- hashmap.js
+++ hashmap.js
@@ -5,7 +5,20 @@
  * Homepage: https://github.com/flesler/hashmap
  */
 
-;(function(exports){
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+  }
+}(this, function () {
 	
 	function HashMap() {
 		this.clear();
@@ -114,6 +127,6 @@
 		}
 	};
 
-	exports.HashMap = HashMap;
+	return HashMap;
 
-})(this.exports || this);
+}));
